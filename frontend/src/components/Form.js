b097ae05fc4a543af81ac0dda1ab81e2eb2d2244
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-11-27 08:33:34 +0100

    functionality completed in Form and started Userlist

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Form = () => {
+
+    //To store the value from Frontend
+    const [userName, setUserName] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    console.log(userName, userEmail);
+
+    //Function to send Data
+    const submitData = async() =>{
+        const data = {
+            name: userName,
+            email: userEmail,
+        }
+        const res = await axios.post("/createUser", data);
+        console.log(res);
+    };
+
+     //To handle default submission
+     const handleSubmit = (event) =>{
+        event.preventDefault();
+        submitData();
+
+      //empty the name and email once submit
+      setUserName("");
+      setUserEmail("");  
+    }
+
+
   return (
-    <div >
-           <div class="my-8">
-              <h1 class="font-medium leading-tight text-4xl mt-0 mb-2 text-purple-800 text-center">Form</h1>
+    <div>
+           <div className="my-8">
+              <h1 className="font-medium leading-tight text-4xl mt-0 mb-2 text-purple-800 text-center">Form</h1>
             </div>
 
-        <div class="block mx-auto p-14 rounded-lg shadow-2xl  border border-solid bg-white text-center  border-gray-200 max-w-sm">    
- <form >
-     <div class="form-group mb-6 ">
-      <input type="text" class="form-control block
+      
+        <div className="block mx-auto p-14 rounded-lg shadow-2xl  border border-solid bg-white text-center  border-gray-200 max-w-sm">    
+ <form onSubmit={handleSubmit}>
+     <div className="form-group mb-6 ">
+      <input type="text" className="form-control block
         w-full
         px-3
         py-1.5
@@ -24,10 +54,12 @@ const Form = () => {
         ease-in-out
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput90"
-        placeholder="Name" />
+        placeholder="Name" 
+        value={userName} onChange={(event) => {setUserName(event.target.value)}}/>
+
     </div>
-    <div class="form-group mb-6">
-      <input type="email" class="form-control block
+    <div className="form-group mb-6">
+      <input type="email" className="form-control block
         w-full
         px-3
         py-1.5
@@ -41,10 +73,11 @@ const Form = () => {
         ease-in-out
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput91"
-        placeholder="Email address" />
+        placeholder="Email address"
+        value={userEmail} onChange={(event)=> {setUserEmail(event.target.value)}} />
     </div>
   
-    <button type="submit" class="
+    <button type="submit" className="
       inline-block
       px-7 
       py-3
--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -2,74 +2,74 @@ import React from 'react'
 
 const UserList = () => {
   return (
-    <div class="mt-[6rem]">
-         <div class="my-6">
-              <h1 class="font-medium leading-tight text-4xl mb-[50px] text-purple-800 text-center">User Lists</h1>
+    <div className="mt-[6rem]">
+         <div className="my-6">
+              <h1 className="font-medium leading-tight text-4xl mb-[50px] text-purple-800 text-center">User Lists</h1>
             </div>
 
-            <div class="flex flex-col shadow-2xl rounded-lg border border-solid border-gray-200 w-3/5 text-center mx-auto">
-        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-      <div class="overflow-hidden">
-        <table class="min-w-full">
-          <thead class="bg-white border-b">
+            <div className="flex flex-col shadow-2xl rounded-lg border border-solid border-gray-200 w-3/5 text-center mx-auto">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+      <div className="overflow-hidden">
+        <table className="min-w-full">
+          <thead className="bg-white border-b">
             <tr>
-              <th scope="col" class="text-2xl font-semibold text-cyan-700 px-6 py-4 text-center">
+              <th scope="col" className="text-2xl font-semibold text-cyan-700 px-6 py-4 text-center">
                 Name
               </th>
              
-              <th scope="col" class="text-2xl font-semibold text-cyan-700 px-6 py-4 text-center">
+              <th scope="col" className="text-2xl font-semibold text-cyan-700 px-6 py-4 text-center">
                 Email
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-gray-100 border-b  ">
-              <td class="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+            <tr className="bg-gray-100 border-b  ">
+              <td className="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
                 Shiva
               </td>
-              <td class="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+              <td className="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
                 @mdo
               </td>
-              <div class="inline-flex">
-  <button class="bg-teal-600  hover:bg-teal-700 text-white font-bold py-2 px-7 mr-2 mt-2 rounded">
+              <div className="inline-flex">
+  <button className="bg-teal-600  hover:bg-teal-700 text-white font-bold py-2 px-7 mr-2 mt-2 rounded">
     Edit
   </button>
-  <button class="bg-red-400 hover:bg-red-500 text-gray-800 font-bold py-2 px-4 mt-2 rounded">
+  <button className="bg-red-400 hover:bg-red-500 text-gray-800 font-bold py-2 px-4 mt-2 rounded">
     Delete
   </button>
 </div>
             </tr>
 
-            <tr class="bg-white border-b">
-              <td class="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+            <tr className="bg-white border-b">
+              <td className="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
                 Jacob
               </td>
              
-              <td class="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+              <td className="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
                 @fat
               </td>
-              <div class="inline-flex">
-  <button class="bg-teal-600 hover:bg-teal-700 text-white font-bold px-7 mr-2 mt-2 rounded">
+              <div className="inline-flex">
+  <button className="bg-teal-600 hover:bg-teal-700 text-white font-bold px-7 mr-2 mt-2 rounded">
     Edit
   </button>
-  <button class="bg-red-400 hover:bg-red-500 text-gray-800 font-bold py-2 px-4 mt-2 rounded">
+  <button className="bg-red-400 hover:bg-red-500 text-gray-800 font-bold py-2 px-4 mt-2 rounded">
     Delete
   </button>
 </div>
             </tr>
-            <tr class="bg-gray-100 border-b">
-            <td class="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+            <tr className="bg-gray-100 border-b">
+            <td className="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
                 kalai
               </td>
-              <td class="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+              <td className="text-base text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
                 @twitter
               </td>
-              <div class="inline-flex">
-  <button class="bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-7 mr-2 mt-2 rounded">
+              <div className="inline-flex">
+  <button className="bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-7 mr-2 mt-2 rounded">
     Edit
   </button>
-  <button class="bg-red-400 hover:bg-red-500 text-gray-800 font-bold py-2 px-4 mt-2 rounded">
+  <button className="bg-red-400 hover:bg-red-500 text-gray-800 font-bold py-2 px-4 mt-2 rounded">
     Delete
   </button>
 </div>
